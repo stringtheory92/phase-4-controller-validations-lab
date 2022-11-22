a131dc186f0c3ce8aa1ab2ebaf240bf7620675ce
Adam Kabak <adamkabak@yahoo.com>
2022-11-22 07:45:11 -0500

    commit tree 653cf21f52b8c9921b29862a62ffee9307d0c3a7

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,9 @@
 class PostsController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :category_validation
+
+  def index
+    render json: Post.all
+  end
 
   def show
     post = Post.find(params[:id])
@@ -9,7 +14,7 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    post.update!(post_params)
 
     render json: post
   end
@@ -18,6 +23,11 @@ class PostsController < ApplicationController
 
   def post_params
     params.permit(:category, :content, :title)
+  end
+
+  def category_validation(invalid)
+        render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+    
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 100}
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+    # validate :category_validation
+
+    # def category_validation
+    #     unless (category == "Fiction" || category == "Non-Fiction")
+    #         byebug
+    #         errors.add(:category, "invalid category")
+    #         render json: errors, status: :unprocessable_entity
+    #     end
+    # end
 end
